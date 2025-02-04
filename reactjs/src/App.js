2e42359aba56b6f2c22eab1a05b1583c9441ca95
YunanLiu06 <liu4807@gmail.com>
2025-02-04 15:52:24 -0500

    add first files

---
+++ reactjs/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header/generalHeader';
+import MainBody from './body';
+import Footer from './footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainBody />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs/src/header/generalHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class="container" style={{position:'fixed', backgroundColor: 'white', width:'100%'}}>
+        <div class="jumbotron">
+            <h1>This is a header</h1>
+            <p>This is the test header for this specific project.</p>
+        </div>
+        </div>
+    )
+};
+
+export default Header;
