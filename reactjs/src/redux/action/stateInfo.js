5e5a450dfd151a8b71bf9fd7bbd50431220da642
YunanLiu06 <liu4807@gmail.com>
2025-04-03 12:24:21 -0400

    update redux logic and gallery height

--- reactjs/src/redux/action/stateInfo.js
+++ reactjs/src/redux/action/stateInfo.js
@@ -1,4 +1,5 @@
 import { ACTIONS } from "../reducer/stateInfo";
+import { S3AccessTool } from "../../tools/s3AccessTool";
 
 export const updateStateInfo = payload => {
   return {
@@ -8,5 +9,24 @@ export const updateStateInfo = payload => {
 };
 
 export const setUpdateStateInfo = (information) => {
-  return (dispatch) => {dispatch(updateStateInfo(information));};
+  return (dispatch) => {
+    dispatch({type: ACTIONS.UPDATE_START});
+    return S3AccessTool({ operation: 'getAll' })
+      .then(response => {
+        const statesInfo = new Map();
+        response?.data?.body.forEach(value => {
+          const currKey = value?.Key;
+          const subKey = currKey.substring(0,2);
+          if(!statesInfo.has(subKey)) {
+            statesInfo.set(subKey, []);
+          }
+          statesInfo.get(subKey).push(currKey);
+        })
+
+        dispatch({
+          type: ACTIONS.UPDATE_STATUS,
+          data: Object.fromEntries(statesInfo)
+        })
+      });
+  };
 };
--- reactjs/src/redux/reducer/stateInfo.js
+++ reactjs/src/redux/reducer/stateInfo.js
@@ -1,15 +1,23 @@
 export const ACTIONS = {
   UPDATE_STATUS: 'UPDATE_STATUS',
+  UPDATE_START: 'UPDATE_START'
 };
 
-const initialState = {};
+const initialState = {inprogress: false};
 
 export const stateInfo = (store = initialState, payload) => {
   switch (payload.type) {
     case ACTIONS.UPDATE_STATUS:
       return {
         ...store,
+        inprogress:false,
         ...payload.data
+      }
+    
+    case ACTIONS.UPDATE_START:
+      return {
+        ...store,
+        inprogress:true
       }
 
     default:
